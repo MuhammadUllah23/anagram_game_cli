2093716742e10c5f52934bb116ee9e365f454bc1
Muhammad <muhammadhullah23@gmail.com>
2021-04-06 03:37:40 -0400

    description method

--- lib/anagram_game_cli/cli.rb
+++ lib/anagram_game_cli/cli.rb
@@ -1,5 +1,21 @@
 class CLI
+
+    def start
+        puts "Please enter your name to create profile."
+        name = user_input
+        description(name)
+
+    end
     def user_input
         gets.strip
     end
+
+    def description(name)
+        puts "Welcome to the anagram game #{name}. This is where you can put your wordsmith skills to the test." 
+        puts "----------------------------------------------------------------------------------------------------"
+        puts "If you would like to see the rules, then please enter rules."
+        puts "If you would like to start the game, then please enter start."
+    end
+
+    
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -3,5 +3,5 @@
 require 'bundler'
 Bundler.require
 
-
+require_all './lib/anagram_game_cli'
 
